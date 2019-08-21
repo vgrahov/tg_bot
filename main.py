e3d6bf39ff1c9ff9178760556d21817d1156a5e8
vgrahov <grahov.vn@gmail.com>
2019-08-21 17:40:10 +0500

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from flask import Flask, request
 import telebot
 
 
-
 token = config.token
-
 bot = telebot.TeleBot(token)
-
 server = Flask(__name__)
 
 
@@ -15,17 +12,19 @@ server = Flask(__name__)
 def start(message):
     bot.reply_to(message, 'Some message to ' + message.fr)
 
+
 @server.route('/'+token, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
 
+
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=config.WEBHOOK_URL_BASE+config.WEBHOOK_URL_PATH)
+    bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH)
     return "!", 200
 
-if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=443)
+    if __name__ == '__main__':
+        server.run(host="0.0.0.0", port=443)
 
 #TODO: make some functionals
